e9205f6c50abc6a6cc76f4a9c809135eada05aa1
Hamlet <hamletgeraldino11@hotmail.com>
2024-05-02 00:02:40 -0400

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Blazor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int zapato = 300;
+
+        if (zapato < 200)
+        {
+            int descuento = zapato - 50;
+            Console.WriteLine("Precio total con el descuento aplicado: " + zapato);
+        }
+        else if (zapato >= 250)
+        {
+            int descuento = zapato - 25;
+            Console.WriteLine("Precio total con el descuento aplicado: " + zapato);
+
+        }
+        else
+        {
+            Console.WriteLine("No hay na pai");
+        }
+
+    }
+}
