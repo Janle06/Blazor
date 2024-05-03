9e7cf7da2e8b54c81588200be6b9e9f67f97daf0
Hamlet Geraldino <130248935+Janle06@users.noreply.github.com>
2024-05-03 03:25:51 -0400

    Practica clase 1

--- Program.cs
+++ Program.cs
@@ -1,15 +1,189 @@
-﻿namespace Proyecto3;
+﻿namespace Practica;
 
 class Program
 {
+    //Datos primarios o primitivos, declaracion de variables
+    /*
     static void Main(string[] args)
     {
-        int zapato = 300;
 
-        if (zapato < 200)
-        {
-            int descuento = zapato - 50;
-            Console.WriteLine("Precio total con el descuento aplicado: " + zapato);
-        }
+        int Edad = 20;
+        string Nombre = "Hamlet Geraldino";
+        double Estatura = 1.92;
+        bool Estudiante = true;
+
+        Console.WriteLine("Edad: " + Edad);
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Estatura: " + Estatura);
+
     }
+    */
+
+
+    //Operaciones aritmeticas
+    /*
+    static void Main(string[] args)
+    {
+
+        int platano = 17;
+        int precio = 15;
+        int multiplicacion = platano * precio;
+        Console.WriteLine("Multiplicacion: " + multiplicacion);
+
+        int pedazos = 12;
+        int personas = 4;
+        int division = pedazos / personas;
+        Console.WriteLine("Division: " + division);
+
+        int años = 27;
+        int añadir = 10;
+        int suma = años + añadir;
+        Console.WriteLine("Suma: " + suma);
+
+        int deuda = 50;
+        int pago = 30;
+        int resta = deuda - pago;
+        Console.WriteLine("Resta: " + resta);
+    }
+    */
+
+
+    //Estructura de control
+    /*
+    static void Main(string[] args)
+    {
+
+        int precio = 100;
+        if (precio > 200)
+        {
+
+            int descuento = precio - 15;
+            Console.WriteLine("El precio con el descuento aplicado: " + descuento);
+
+        }
+        else if (precio <= 150)
+        {
+
+            int descuento = precio - 10;
+            Console.WriteLine("El precio con el descuento aplicado: " + descuento);
+
+        }
+        else
+        {
+            Console.WriteLine("No hay decuento pai");
+        }
+
+    }
+    */
+
+
+    /*
+    static void Main(string[] args)
+    {
+
+        int dia = 5;
+
+        switch (dia)
+        {
+
+            case 1:
+                Console.WriteLine("Lunes");
+                break;
+            case 2:
+                Console.WriteLine("Martes");
+                break;
+            case 3:
+                Console.WriteLine("Miercoles");
+                break;
+            case 4:
+                Console.WriteLine("Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Viernes");
+                break;
+            case 6:
+                Console.WriteLine("Sabado");
+                break;
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+            default:
+                Console.WriteLine("No existe ese dia");
+                break;
+
+        }
+
+    }
+    */
+
+
+    //Bucle for 
+    /*
+    static void Main(string[] args)
+    {
+        /*
+        //Bucle del 1 - 5
+        for (int i = 0; i < 5; i++) 
+        {
+            Console.WriteLine(i);
+        }
+        */
+
+    //Bucle del 5 - 1
+    /*
+    for (int i = 5; i > 0; i--)
+    {
+        Console.WriteLine(i);
+    }
+    */
+
+    /*
+    //Bucle for de 5 en 5
+    for (int i = 0; i <= 50; i = i + 5)
+    {
+        Console.WriteLine(i);
+    }
+
 }
+*/
+
+
+    //Bucle while
+    /*
+    static void Main(string[] args)
+
+    {
+
+        int i = 5;
+        while (i < 21)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+
+    }
+    */
+
+    //Arrays
+    //Pa guardar multiple informacion en una sola variable miop
+    /*
+    static void Main(string[] args)
+    {
+        
+        string[] drogas = { "Percocet", "Molly", "Weed" };
+        drogas[2] = "Crack";
+        Console.WriteLine(drogas[2]);
+        
+
+        //Otra forma de hacer Arrays
+        
+        string[] cars = new string[4];
+        cars = new string[] { "Ferrari", "Lamborghini", "BMW", "Hyundai" };
+        Console.WriteLine(cars[1]);
+        
+    }
+    */
+
+
+}
+
