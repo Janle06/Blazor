d43b469fe5d21179adbe36009479aaa41e0a0c33
Hamlet Geraldino <130248935+Janle06@users.noreply.github.com>
2024-05-08 02:14:55 -0400

    Carteleta (Submenus implementado)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace Practica;
 
+/*
 class Program
 {
     //Datos primarios o primitivos, declaracion de variables
@@ -20,170 +21,167 @@ class Program
     */
 
 
-    //Operaciones aritmeticas
-    /*
-    static void Main(string[] args)
+//Operaciones aritmeticas
+/*
+static void Main(string[] args)
+{
+
+    int platano = 17;
+    int precio = 15;
+    int multiplicacion = platano * precio;
+    Console.WriteLine("Multiplicacion: " + multiplicacion);
+
+    int pedazos = 12;
+    int personas = 4;
+    int division = pedazos / personas;
+    Console.WriteLine("Division: " + division);
+
+    int años = 27;
+    int añadir = 10;
+    int suma = años + añadir;
+    Console.WriteLine("Suma: " + suma);
+
+    int deuda = 50;
+    int pago = 30;
+    int resta = deuda - pago;
+    Console.WriteLine("Resta: " + resta);
+}
+*/
+
+
+//Estructura de control
+/*
+static void Main(string[] args)
+{
+
+    int precio = 100;
+    if (precio > 200)
     {
 
-        int platano = 17;
-        int precio = 15;
-        int multiplicacion = platano * precio;
-        Console.WriteLine("Multiplicacion: " + multiplicacion);
-
-        int pedazos = 12;
-        int personas = 4;
-        int division = pedazos / personas;
-        Console.WriteLine("Division: " + division);
-
-        int años = 27;
-        int añadir = 10;
-        int suma = años + añadir;
-        Console.WriteLine("Suma: " + suma);
-
-        int deuda = 50;
-        int pago = 30;
-        int resta = deuda - pago;
-        Console.WriteLine("Resta: " + resta);
-    }
-    */
-
-
-    //Estructura de control
-    /*
-    static void Main(string[] args)
-    {
-
-        int precio = 100;
-        if (precio > 200)
-        {
-
-            int descuento = precio - 15;
-            Console.WriteLine("El precio con el descuento aplicado: " + descuento);
-
-        }
-        else if (precio <= 150)
-        {
-
-            int descuento = precio - 10;
-            Console.WriteLine("El precio con el descuento aplicado: " + descuento);
-
-        }
-        else
-        {
-            Console.WriteLine("No hay decuento pai");
-        }
+        int descuento = precio - 15;
+        Console.WriteLine("El precio con el descuento aplicado: " + descuento);
 
     }
-    */
-
-
-    /*
-    static void Main(string[] args)
+    else if (precio <= 150)
     {
 
-        int dia = 5;
-
-        switch (dia)
-        {
-
-            case 1:
-                Console.WriteLine("Lunes");
-                break;
-            case 2:
-                Console.WriteLine("Martes");
-                break;
-            case 3:
-                Console.WriteLine("Miercoles");
-                break;
-            case 4:
-                Console.WriteLine("Jueves");
-                break;
-            case 5:
-                Console.WriteLine("Viernes");
-                break;
-            case 6:
-                Console.WriteLine("Sabado");
-                break;
-            case 7:
-                Console.WriteLine("Domingo");
-                break;
-            default:
-                Console.WriteLine("No existe ese dia");
-                break;
-
-        }
+        int descuento = precio - 10;
+        Console.WriteLine("El precio con el descuento aplicado: " + descuento);
 
     }
-    */
-
-
-    //Bucle for 
-    /*
-    static void Main(string[] args)
+    else
     {
-        /*
-        //Bucle del 1 - 5
-        for (int i = 0; i < 5; i++) 
-        {
-            Console.WriteLine(i);
-        }
-        */
-
-    //Bucle del 5 - 1
-    /*
-    for (int i = 5; i > 0; i--)
-    {
-        Console.WriteLine(i);
-    }
-    */
-
-    /*
-    //Bucle for de 5 en 5
-    for (int i = 0; i <= 50; i = i + 5)
-    {
-        Console.WriteLine(i);
+        Console.WriteLine("No hay decuento pai");
     }
 
 }
 */
 
 
-    //Bucle while
-    /*
-    static void Main(string[] args)
+/*
+static void Main(string[] args)
+{
 
+    int dia = 5;
+
+    switch (dia)
     {
 
-        int i = 5;
-        while (i < 21)
-        {
-            Console.WriteLine(i);
-            i++;
-        }
+        case 1:
+            Console.WriteLine("Lunes");
+            break;
+        case 2:
+            Console.WriteLine("Martes");
+            break;
+        case 3:
+            Console.WriteLine("Miercoles");
+            break;
+        case 4:
+            Console.WriteLine("Jueves");
+            break;
+        case 5:
+            Console.WriteLine("Viernes");
+            break;
+        case 6:
+            Console.WriteLine("Sabado");
+            break;
+        case 7:
+            Console.WriteLine("Domingo");
+            break;
+        default:
+            Console.WriteLine("No existe ese dia");
+            break;
 
     }
-    */
-
-    //Arrays
-    //Pa guardar multiple informacion en una sola variable miop
-    /*
-    static void Main(string[] args)
-    {
-        
-        string[] drogas = { "Percocet", "Molly", "Weed" };
-        drogas[2] = "Crack";
-        Console.WriteLine(drogas[2]);
-        
-
-        //Otra forma de hacer Arrays
-        
-        string[] cars = new string[4];
-        cars = new string[] { "Ferrari", "Lamborghini", "BMW", "Hyundai" };
-        Console.WriteLine(cars[1]);
-        
-    }
-    */
-
 
 }
+*/
+
+
+//Bucle for 
+/*
+static void Main(string[] args)
+{
+    /*
+    //Bucle del 1 - 5
+    for (int i = 0; i < 5; i++) 
+    {
+        Console.WriteLine(i);
+    }
+    */
+
+//Bucle del 5 - 1
+/*
+for (int i = 5; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+*/
+
+/*
+//Bucle for de 5 en 5
+for (int i = 0; i <= 50; i = i + 5)
+{
+    Console.WriteLine(i);
+}
+
+}
+*/
+
+
+//Bucle while
+/*
+static void Main(string[] args)
+
+{
+
+    int i = 5;
+    while (i < 21)
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+
+}
+*/
+
+//Arrays
+//Pa guardar multiple informacion en una sola variable miop
+/*
+static void Main(string[] args)
+{
+
+    string[] drogas = { "Percocet", "Molly", "Weed" };
+    drogas[2] = "Crack";
+    Console.WriteLine(drogas[2]);
+
+
+    //Otra forma de hacer Arrays
+
+    string[] cars = new string[4];
+    cars = new string[] { "Ferrari", "Lamborghini", "BMW", "Hyundai" };
+    Console.WriteLine(cars[1]);
+
+}
+*/
 
--- cartelera.cs
+++ cartelera.cs
@@ -17,104 +17,260 @@ class Tarea1
 
         //Tipos y precios de boletas
         double BoletaRegular = 125; //Precio de boleto
-        double Boleta3D = 350; //Precio de boleto
-        double BoletaVIP = 525; //Precio de boleto
-        double BoletaCombo = 650; //Precio de boleto
+        double Boleta3D = 325; //Precio de boleto
+        double BoletaVIP = 500; //Precio de boleto
         double PrecioTotal = 0; //El Precio total de todas las boletas
+
+        //Combos de comida y sus precios
+        double Combo1 = 325; //Combo de palomitas con refresco
+        double Combo2 = 325; //Combo de hotdog con refresco
+        double Combo3 = 325; //Combo de nachos con queso y refresco
+
+        //Balance para agregar
+        double Saldo1 = 400; //Para agregar 400 pesos al balance
+        double Saldo2 = 800; //Para agregar 800 pesos al balance
+        double Saldo3 = 1200; //Para agregar 1200 pesos al balance
 
         //Cantidad de tickets
         List<int> Tickets = new List<int>();
+        List<int> Combos = new List<int>();
 
-        bool salir = true;
+
+
+
         //Generador de escritura
         string filepath = "Cartelera.txt";
         using (StreamWriter generadorEscritura = new StreamWriter(filepath))
         {
 
+            //Interfaz de la cartelera 
+            Console.WriteLine("---------------------");
+            Console.WriteLine("------CARTELERA------");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Balance disponible: " + Balance);
+            Console.WriteLine("Por favor seleccione una opcion:");
+            Console.WriteLine("1. Compra de Boleto");
+            Console.WriteLine("2. Compra de Combo");
+            Console.WriteLine("3. Agregar Balance");
+            Console.WriteLine("4. Salir");
+            int MenuSeleccion = int.Parse(Console.ReadLine()!);
+
+            bool salir = true;
             while (salir)
             {
-
-                //Interfaz de la cartelera 
-                Console.WriteLine("---Cartelera---");
-                Console.WriteLine("Por favor seleccione una opcion:");
-                Console.WriteLine("1. Boleto Regular");
-                Console.WriteLine("2. Boleto 3D");
-                Console.WriteLine("3. Boleto VIP");
-                Console.WriteLine("4. Boleto con Combo");
-                Console.WriteLine("5. Salir");
-                int SeleccionBoleto = int.Parse(Console.ReadLine()!);
-
-                switch (SeleccionBoleto)
+                bool volver = false;
+                while (!volver)
                 {
-                    case 1:
-                        if (Balance >= BoletaRegular)
-                        {
-                            Balance -= BoletaRegular;
-                            PrecioTotal += BoletaRegular;
-                            Console.WriteLine("Boleta regular agregada");
-                            generadorEscritura.WriteLine("Boleta regular agregada");
-                            Tickets.Add(1);
-                        }
-                        else
-                        {
-                            Console.WriteLine("No tienes suficiente balance");
-                        }
-                        break;
-                    case 2:
-                        if (Balance >= Boleta3D)
-                        {
-                            Balance -= Boleta3D;
-                            PrecioTotal += Boleta3D;
-                            Console.WriteLine("Boleta 3D agregada");
-                            generadorEscritura.WriteLine("Boleta 3D agregada");
-                            Tickets.Add(1);
+                    switch (MenuSeleccion)
+                    {
 
-                        }
-                        else
-                        {
-                            Console.WriteLine("No tienes suficiente balance");
-                        }
-                        break;
-                    case 3:
-                        if (Balance >= BoletaVIP)
-                        {
-                            Balance -= BoletaVIP;
-                            PrecioTotal += BoletaVIP;
-                            Console.WriteLine("Boleta VIP agregada");
-                            generadorEscritura.WriteLine("Boleta VIP agregada");
-                            Tickets.Add(1);
-                        }
-                        else
-                        {
-                            Console.WriteLine("No tienes suficiente balance");
-                        }
-                        break;
-                    case 4:
-                        if (Balance >= BoletaCombo)
-                        {
-                            Balance -= BoletaCombo;
-                            PrecioTotal += BoletaCombo;
-                            Console.WriteLine("Boleta con Combo agregada");
-                            generadorEscritura.WriteLine("Boleta con Combo agregada");
-                            Tickets.Add(1);
-                        }
-                        else
-                        {
-                            Console.WriteLine("No tienes suficiente balance");
-                        }
-                        break;
-                    case 5:
-                        salir = false;
-                        break;
-                    default:
-                        Console.WriteLine("Introduzca una opcion valida..");
-                        break;
+                        case 1:
+                            {
+                                Console.WriteLine("---------------------");
+                                Console.WriteLine("------Boleteria------");
+                                Console.WriteLine("---------------------");
+                                Console.WriteLine("1. Boleta Regular");
+                                Console.WriteLine("2. Boleta 3D");
+                                Console.WriteLine("3. Boleta VIP");
+                                Console.WriteLine("4. Volver al menu");
+                                int SeleccionBoleto = int.Parse(Console.ReadLine()!);
+
+                                switch (SeleccionBoleto)
+                                {
+                                    case 1:
+                                        if (Balance >= BoletaRegular)
+                                        {
+                                            Balance -= BoletaRegular;
+                                            PrecioTotal += BoletaRegular;
+                                            Console.WriteLine("Boleta regular agregada");
+                                            generadorEscritura.WriteLine("Boleta regular agregada");
+                                            Tickets.Add(1);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("No tienes suficiente balance");
+                                        }
+                                        break;
+                                    case 2:
+                                        if (Balance >= Boleta3D)
+                                        {
+                                            Balance -= Boleta3D;
+                                            PrecioTotal += Boleta3D;
+                                            Console.WriteLine("Boleta 3D agregada");
+                                            generadorEscritura.WriteLine("Boleta 3D agregada");
+                                            Tickets.Add(1);
+
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("No tienes suficiente balance");
+                                        }
+                                        break;
+                                    case 3:
+                                        if (Balance >= BoletaVIP)
+                                        {
+                                            Balance -= BoletaVIP;
+                                            PrecioTotal += BoletaVIP;
+                                            Console.WriteLine("Boleta VIP agregada");
+                                            generadorEscritura.WriteLine("Boleta VIP agregada");
+                                            Tickets.Add(1);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("No tienes suficiente balance");
+                                        }
+                                        break;
+                                    case 4:
+                                        {
+                                            volver = true;
+                                        }
+                                        break;
+                                    default:
+                                        Console.WriteLine("Introduzca una opcion valida..");
+                                        break;
+                                }
+
+                            }
+                            break;
+                        case 2:
+                            {
+                                Console.WriteLine("----------------------");
+                                Console.WriteLine("--------COMBOS--------");
+                                Console.WriteLine("----------------------");
+                                Console.WriteLine("1. Combo de Palomitas con refresco");
+                                Console.WriteLine("2. Combo de Hotdog con refresco");
+                                Console.WriteLine("3. Combo de Nachos con queso y refresco");
+                                Console.WriteLine("4. Volver al menu");
+                                int Cafeteria = int.Parse(Console.ReadLine()!);
+
+                                switch (Cafeteria)
+                                {
+                                    case 1:
+                                        if (Balance >= Combo1)
+                                        {
+                                            Balance -= Combo1;
+                                            PrecioTotal += Combo1;
+                                            Console.WriteLine("Combo de Palomitas con refresco agregado");
+                                            generadorEscritura.WriteLine("Combo de Palomitas con refresco agregado");
+                                            Combos.Add(1);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("No tienes suficiente balance");
+                                        }
+                                        break;
+                                    case 2:
+                                        if (Balance >= Combo2)
+                                        {
+                                            Balance -= Combo2;
+                                            PrecioTotal += Combo2;
+                                            Console.WriteLine("Combo de Hotdog con refresco agregado");
+                                            generadorEscritura.WriteLine("Combo de Hotdog con refresco agregado");
+                                            Combos.Add(1);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("No tienes suficiente balance");
+                                        }
+                                        break;
+                                    case 3:
+                                        if (Balance >= Combo3)
+                                        {
+                                            Balance -= Combo3;
+                                            PrecioTotal += Combo3;
+                                            Console.WriteLine("Combo de Nachos con queso y refresco agregado");
+                                            generadorEscritura.WriteLine("Combo de Nachos con queso y refresco agregado");
+                                            Combos.Add(1);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("No tienes suficiente balance");
+                                        }
+                                        break;
+                                    case 4:
+                                        {
+                                            volver = true;
+                                        }
+                                        break;
+                                    default:
+                                        Console.WriteLine("Introduzca una opcion valida..");
+                                        break;
+                                }
+
+                            }
+                            break;
+                        case 3:
+                            {
+                                Console.WriteLine("-----------------------");
+                                Console.WriteLine("--------BALANCE--------");
+                                Console.WriteLine("-----------------------");
+                                Console.WriteLine("1. Agregar 400 pesos al balance");
+                                Console.WriteLine("2. Agregar 800 pesos al balance");
+                                Console.WriteLine("3. Agregar 1200 pesos al balance");
+                                Console.WriteLine("4. Volver al menu");
+                                int AgregarBalance = int.Parse(Console.ReadLine()!);
+
+                                switch (AgregarBalance)
+                                {
+                                    case 1:
+                                        if (Balance >= Saldo1)
+                                        {
+                                            Balance += Saldo1;
+                                            PrecioTotal += Saldo1;
+                                            Console.WriteLine("400 pesos han sido añadidos a su balance");
+                                            generadorEscritura.WriteLine("400 pesos han sido añadidos a su balance");
+                                        }
+                                        break;
+                                    case 2:
+                                        if (Balance >= Saldo2)
+                                        {
+                                            Balance += Saldo2;
+                                            PrecioTotal += Saldo2;
+                                            Console.WriteLine("800 pesos han sido añadidos a su balance");
+                                            generadorEscritura.WriteLine("800 pesos han sido añadidos a su balance");
+                                        }
+                                        break;
+                                    case 3:
+                                        if (Balance >= Saldo3)
+                                        {
+                                            Balance += Saldo3;
+                                            PrecioTotal += Saldo3;
+                                            Console.WriteLine("1200 pesos han sido añadidos a su balance");
+                                            generadorEscritura.WriteLine("1200 pesos han sido añadidos a su balance");
+                                        }
+                                        break;
+                                    case 4:
+                                        {
+                                            volver = true;
+                                        }
+                                        break;
+                                    default:
+                                        Console.WriteLine("Introduzca una opcion valida..");
+                                        break;
+                                }
+                            }
+                            break;
+                        case 4:
+                            {
+                                salir = false;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Introduzca una opcion valida..");
+                            break;
+
+                    }
+
                 }
             }
+
             generadorEscritura.WriteLine("Su balance es de " + Balance + " pesos" + " y ha comprado " + Tickets.Count + " tickets"
+        + " por un precio total de " + PrecioTotal + " pesos");
+
+            Console.WriteLine("Su balance es de " + Balance + " pesos" + " y ha comprado " + Tickets.Count + " tickets"
             + " por un precio total de " + PrecioTotal + " pesos");
+
         }
-        Console.WriteLine("Su balance es de " + Balance + " pesos" + " y ha comprado " + Tickets.Count + " tickets"
-            + " por un precio total de " + PrecioTotal + " pesos");
+
     }
 }
